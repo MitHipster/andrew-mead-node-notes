c9dd582c763b237d2169c1818b45c0cdb2a3a62e
Tim Acker <tjacker31@gmail.com>
2019-02-02 20:01:17 -0500

    Creates functions in note module and adds yargs library

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
 // const fs = require('fs');
 // const _ = require('lodash');
+const yargs = require('yargs');
 
-// const notes = require('./notes');
+const notes = require('./notes');
 
-let command = process.argv[2];
-console.log('==>: process.argv', process.argv);
+const argv = yargs.argv;
+let command = argv._[0];
+console.log('==>: argv', argv);
 
 switch (command) {
 case 'add':
-	console.log('Add new note');
+	notes.addNote(argv.title, argv.body);
 	break;
 case 'remove':
-	console.log('Remove a note');
+	notes.removeNote(argv.title);
 	break;
 case 'list':
-	console.log('List all notes');
+	notes.getAll();
 	break;
 case 'read':
-	console.log('Read a note');
+	notes.getNote(argv.title);
 	break;
 
 default:
-	console.log('Command not recognized');
+	console.log('==>: Command not recognized');
 	break;
 }
--- notes.js
+++ notes.js
@@ -1,7 +1,25 @@
-console.log('Starting notes.js');
+console.log('==>: Starting notes.js');
 
-// console.log(module);
-module.exports.addNote = () => {
-	console.log('addNote');
-	return 'New Note';
+let addNote = (title, body) => {
+	console.log('==>: addNote -> title', title);
+	console.log('==>: addNote -> body', body);
+};
+
+let removeNote = title => {
+	console.log('==>: removeNote -> title', title);
+};
+
+let getAll = () => {
+	console.log('==>: Getting all notes');
+};
+
+let getNote = title => {
+	console.log('==>: getNote -> title', title);
+};
+
+module.exports = {
+	addNote,
+	removeNote,
+	getAll,
+	getNote
 };
